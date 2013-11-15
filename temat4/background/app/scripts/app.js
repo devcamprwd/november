1cf7e3bacbd20424b3bbb2be1ab7227aa57e8c28
kasprownik <kasper.mowgli@gmail.com>
2013-11-15 19:33:30 +0100

    whatever

--- temat4/background/app/scripts/app.js
+++ temat4/background/app/scripts/app.js
@@ -2,5 +2,14 @@
 define([], function () {
     'use strict';
 
-    return '\'Allo \'Allo!';
+    if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+        var msViewportStyle = document.createElement("style");
+        msViewportStyle.appendChild(
+            document.createTextNode(
+                "@-ms-viewport{width:auto!important}"
+            )
+        );
+        document.getElementsByTagName("head")[0].
+            appendChild(msViewportStyle);
+    }
 });
