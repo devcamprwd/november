e0e09ca6d697707e1d13e6b9428f302099ddca54
kasprownik <kasper.mowgli@gmail.com>
2013-11-15 11:34:37 +0100

    toggled animations, multiply elements

--- temat3/jscss/js/main.js
+++ temat3/jscss/js/main.js
@@ -1,1 +1,38 @@
+$(function () {
+    var $cubes = $('.cube'),
+        $playground = $('.experiment'),
+        $body = $('body'),
+        times = 0,
+        animateTimer;
 
+    function animate() {
+
+        $cubes.removeClass('is-active');
+
+        $('.experiment').each(function () {
+            $(this).find('.cube').eq(times % 3).addClass('is-active');
+        });
+
+        times += 1;
+        animateTimer = setTimeout(animate, 1000);
+    }
+
+    function toggleAnimation() {
+        if (animateTimer) {
+            clearTimeout(animateTimer);
+            animateTimer = undefined;
+        } else {
+            animate();
+        }
+    }
+
+    function multiplyElements() {
+        console.log($playground.clone());
+        $body.append($playground.clone());
+        $cubes = $('.cube');
+    }
+
+
+    $('#toggle-animation').on('click', toggleAnimation);
+    $('#multiply-elements').on('click', multiplyElements);
+});
