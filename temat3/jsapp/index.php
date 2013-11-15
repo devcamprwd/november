320582447f91e70dfc7e87412cf274a34fce2a07
Timi <timi@timiportfolio.com>
2013-11-15 18:21:48 +0100

    test3 jsapp added status

--- temat3/jsapp/index.php
+++ temat3/jsapp/index.php
@@ -14,9 +14,12 @@
 			-webkit-transform: translate3d(0,0,0);
 			transform: translate3d(0,0,0);
 		}
+		.status { background: lime; padding: 1rem; }
+		.status.off { background: tomato; }
 	</style>
 </head>
 <body>
+	<div class="status">translate3d on</div>
 	<?php 
 		for( $i = 1; $i <= 600; $i++ ) {
 			echo '<div class="box"><img src="images/face.jpg" width="100" height="100" alt=""></div>';
@@ -27,6 +30,7 @@
 		setTimeout(function() {
 
     		$('body').find('.box').removeClass('box');
+    		$('.status').text('translate3d off').addClass('off');
 
     	}, 2000);
 	</script>
