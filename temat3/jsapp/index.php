8bfa1092f25d42bb152d3277d86734a20fe19096
Timi <timi@timiportfolio.com>
2013-11-15 18:18:01 +0100

    test3 jsapp

--- temat3/jsapp/index.php
+++ temat3/jsapp/index.php
@@ -7,51 +7,28 @@
 	<meta http-equiv="cache-control" content="no-cache">
 	<meta http-equiv="pragma" content="no-cache">
 	<style>
-
-		body,
-		p { margin: 0; }
+		body { margin: 0; }
+		img { display: block; }
 
 		.box {
 			-webkit-transform: translate3d(0,0,0);
 			transform: translate3d(0,0,0);
-
-			/*-webkit-transform: translate(0,0);
-			transform: translate(0,0);*/
 		}
-
-		img { display: block; }
-	
 	</style>
 </head>
 <body>
 	<?php 
-		
-		for( $i = 1; $i <= 200; $i++ ) {
-
-			echo '<div class="box"><img src="images/guzik.jpg" width="100" height="100" alt=""></div>';
-
+		for( $i = 1; $i <= 600; $i++ ) {
+			echo '<div class="box"><img src="images/face.jpg" width="100" height="100" alt=""></div>';
 		}
-
 	?>
-	
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script>
-
 		setTimeout(function() {
 
     		$('body').find('.box').removeClass('box');
 
-
     	}, 2000);
-
-    	
-   	
-
 	</script>
-	
-
-	
-	
-	
 </body>
 </html>
