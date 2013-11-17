408a08b8126f884833958c778ddf1ac0e87d216f
Timi <timi@timiportfolio.com>
2013-11-17 09:43:19 +0100

    cssregions

--- cssregions/Gruntfile.js
+++ cssregions/Gruntfile.js
@@ -59,7 +59,7 @@ module.exports = function (grunt) {
             options: {
                 port: 9000,
                 // change this to '0.0.0.0' to access the server from outside
-                hostname: 'localhost'
+                hostname: '0.0.0.0'
             },
             livereload: {
                 options: {
@@ -95,7 +95,7 @@ module.exports = function (grunt) {
         },
         open: {
             server: {
-                path: 'http://localhost:<%= connect.options.port %>'
+                path: 'http://0.0.0.0:<%= connect.options.port %>'
             }
         },
         clean: {
@@ -126,7 +126,7 @@ module.exports = function (grunt) {
             all: {
                 options: {
                     run: true,
-                    urls: ['http://localhost:<%= connect.options.port %>/index.html']
+                    urls: ['http://0.0.0.0:<%= connect.options.port %>/index.html']
                 }
             }
         },
